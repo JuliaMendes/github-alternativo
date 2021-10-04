e3fa15d658b582317de2e7b1e879830f38ae648b
JuliaMendes <juliadepaivams@gmail.com>
2021-10-04 14:41:49 -0300

    consumindo a api do github

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import apiGitHub from './services/apiGithub';
 
 function App() {
+  const [user, setUser] = useState({});
+  const [pesquisa, setPesquisa] = useState('');
 
   useEffect(() => {
-    apiGitHub.get('users/carvalholeo')
-      .then(response => response.data)
-      .then(resposta => console.log(resposta))
-      .catch(erro => console.error(erro))
+    apiGitHub.get('user')
+    .then(response => response.data)
+    .then(console.log)
   }, [])
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    apiGitHub.get('users/' + pesquisa)
+      .then(response => response.data)
+      .then(resposta => setUser(resposta))
+      .catch(erro => console.error(erro))
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="usuario">
+            <input type="text" name="" id="usuario" value={pesquisa} onChange={e => setPesquisa(e.target.value)} />
+          </label>
+          <button type="submit">Pesquisar</button>
+        </form>
+
+        <p>Usuário: {user.login}</p>
+        <img src={user.avatar_url} alt="" />
+        <p>Biografia: {user.bio}</p>
+      </div>
     </div>
   );
 }
--- src/services/apiGithub.js
+++ src/services/apiGithub.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const apiGitHub = axios.create({
-    baseURL: 'https://api.github.com'
+    baseURL: 'https://api.github.com',
+    headers: {
+        Authorization: 'token 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'
+    }
 });
 
 export default apiGitHub;
